61594ece0bcc15d4a4f727dc09d8955b58e72a7b
Tom Sell <thomas.j.sell@gmail.com>
2015-01-27 19:22:06 -0500

    initial commit

---
+++ src/gymcomp.c
@@ -0,0 +1,73 @@
+#include <pebble.h>
+
+#define VIEW 0
+#define EDIT 1
+
+static Window *window;
+static TextLayer *text_layer;
+int mode;
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Mode: %i", mode);
+  if(mode){
+    mode = VIEW;
+    text_layer_set_text(text_layer, "VIEW");
+  }else{
+    mode = EDIT;
+    text_layer_set_text(text_layer, "EDIT");
+  }
+}
+
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Up");
+}
+
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Down");
+}
+
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  text_layer = text_layer_create((GRect) { .origin = { 0, 30 }, .size = { bounds.size.w, bounds.size.h } });
+  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text(text_layer, "GYM COMP");
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
+
+static void window_unload(Window *window) {
+  text_layer_destroy(text_layer);
+}
+
+static void init(void) {
+  mode = VIEW;
+  window = window_create();
+  window_set_click_config_provider(window, click_config_provider);
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  const bool animated = true;
+  window_stack_push(window, animated);
+}
+
+static void deinit(void) {
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+
+  app_event_loop();
+  deinit();
+}
---
+++ src/weights.c
@@ -0,0 +1,15 @@
+
+struct Weights
+{
+  char name[10];
+  int bench;
+  int back;
+  int biceps;
+  int triceps;
+  int shoulder;
+  int ab;
+  int quads;
+  int hammies;
+  int calves;
+  int legpress;
+};
